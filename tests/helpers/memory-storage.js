5e6f53dd90508280a8191848faa2dbed37eddfa4
Dmytro Chernov <chernov.dmtr@gmail.com>
2026-08-20 20:09:29 +0300

    cleanup main

--- tests/helpers/memory-storage.js
+++
@@ -1,9 +0,0 @@
-export function createMemoryStorage(seed = {}) {
-  const values = new Map(Object.entries(seed));
-  return {
-    getItem: key => values.has(key) ? values.get(key) : null,
-    setItem: (key, value) => values.set(key, String(value)),
-    removeItem: key => values.delete(key),
-    dump: () => Object.fromEntries(values)
-  };
-}
